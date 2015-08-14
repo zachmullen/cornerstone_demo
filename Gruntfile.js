5f418e779f39ad8409b879102f0b1b9a037b8b18
Zach Mullen <zach.mullen@kitware.com>
2015-08-14 11:07:40 -0400

    Use cornerstoneWADOImageLoader
    Right now just shows first file from the item

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,27 @@
 module.exports = function (grunt) {
-    var target = 'clients/web/static/built/plugins/cornerstone_demo/plugin.min.js';
-    var jsFiles = grunt.config.get('uglify.plugin_cornerstone_demo.files');
+    (function () {
+        var target = 'clients/web/static/built/plugins/cornerstone_demo/plugin.min.js';
+        var jsFiles = grunt.config.get('uglify.plugin_cornerstone_demo.files');
 
-    jsFiles[target] = [
-        'plugins/cornerstone_demo/bower_components/dicomParser/dist/dicomParser.js',
-        'plugins/cornerstone_demo/bower_components/cornerstone/dist/cornerstone.js',
-        'plugins/cornerstone_demo/bower_components/cornerstoneMath/dist/cornerstoneMath.js',
-        'plugins/cornerstone_demo/bower_components/cornerstoneTools/dist/cornerstoneTools.js'
-    ].concat(jsFiles[target]);
-    grunt.config.set('uglify.plugin_cornerstone_demo.files', jsFiles);
+        jsFiles[target] = [
+            'plugins/cornerstone_demo/bower_components/dicomParser/dist/dicomParser.js',
+            'plugins/cornerstone_demo/bower_components/cornerstone/dist/cornerstone.js',
+            'plugins/cornerstone_demo/bower_components/cornerstoneMath/dist/cornerstoneMath.js',
+            'plugins/cornerstone_demo/bower_components/cornerstoneTools/dist/cornerstoneTools.js',
+            'plugins/cornerstone_demo/bower_components/cornerstoneWADOImageLoader/dist/cornerstoneWADOImageLoader.js'
+        ].concat(jsFiles[target]);
+
+        grunt.config.set('uglify.plugin_cornerstone_demo.files', jsFiles);
+    }) ();
+
+    (function () {
+        var target = 'clients/web/static/built/plugins/cornerstone_demo/plugin.min.css';
+        var cssFiles = grunt.config.get('stylus.plugin_cornerstone_demo.files');
+
+        cssFiles[target] = [
+            'plugins/cornerstone_demo/bower_components/cornerstone/dist/cornerstone.css'
+        ].concat(cssFiles[target]);
+
+        grunt.config.set('stylus.plugin_cornerstone_demo.files', cssFiles);
+    }) ();
 };
--- web_client/js/CornerstoneView.js
+++ web_client/js/CornerstoneView.js
@@ -1,18 +1,24 @@
-(function () {
-    girder.views.cornerstone_demo_CornerstoneView = girder.View.extend({
-        initialize: function (settings) {
-            this.files = settings.files;
-            this.item = settings.item;
-        },
+girder.views.cornerstone_demo_CornerstoneView = girder.View.extend({
+    initialize: function (settings) {
+        this.files = settings.files;
+        this.item = settings.item;
+        this.imageUrls = _.map(this.files.models, function (file) {
+            // HACK see https://github.com/chafey/cornerstoneWADOImageLoader/issues/16
+            return 'dicomweb://' + window.location.host + file.downloadUrl();
+        });
+    },
 
-        render: function () {
-            this.$el.html(girder.templates.cornerstone_demo_viewer());
-            this._initCornerstone();
-        },
+    render: function () {
+        this.$el.html(girder.templates.cornerstone_demo_viewer());
+        this._initCornerstone();
+    },
 
-        _initCornerstone: function () {
-            
-        }
-    });
+    _initCornerstone: function () {
+        var renderer = this.$('.g-render-target')[0];
+        cornerstone.enable(renderer);
 
-})();
+        cornerstone.loadAndCacheImage(this.imageUrls[0]).then(function (image) {
+            cornerstone.displayImage(renderer, image);
+        });
+    }
+});
